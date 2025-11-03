72ec0167d15583dc52a269690b4bab5bf5f27bfe
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-11-03 23:53:08 +0100

    Rename file

--- lib/html_compressor.rb
+++ lib/html_compressor.rb
@@ -2,7 +2,7 @@
 
 module HtmlCompressor
   require_relative 'html_compressor/version'
-  require_relative 'html_compressor/exceptions'
+  require_relative 'html_compressor/errors'
   require_relative 'html_compressor/compressor'
   require_relative 'html_compressor/rack'
 end

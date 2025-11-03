a66ac41643df87865f04b2d6936e4f9bdea369b2
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-11-03 23:51:23 +0100

    Move requires within HtmlCompressor module

--- lib/html_compressor.rb
+++ lib/html_compressor.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-require_relative 'html_compressor/version'
-require_relative 'html_compressor/exceptions'
-require_relative 'html_compressor/compressor'
-require_relative 'html_compressor/rack'
-
 module HtmlCompressor
+  require_relative 'html_compressor/version'
+  require_relative 'html_compressor/exceptions'
+  require_relative 'html_compressor/compressor'
+  require_relative 'html_compressor/rack'
 end
